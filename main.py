41dea81e5b7f4e4f84eab5c7d398c28709db65d5
barrycg <barrycg@users.noreply.github.com>
2019-07-14 20:47:07 +0800

    add samples of network3

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ Created on Sat Jul 13 20:55:58 2019
 
 @author: barry
 """
+
+'''
 import mnist_loader
-
 import network
-
 if __name__ == '__main__':
     
     training_data, validation_data, test_data = \
@@ -16,4 +16,99 @@ if __name__ == '__main__':
     net = network.Network([784, 30, 10])
 
     net.SGD(training_data, 30, 10, 3.0, test_data=test_data)        
+'''
+
+'''
+import mnist_loader    
+import network2
+if __name__ == '__main__':
     
+    training_data, validation_data, test_data = \
+        mnist_loader.load_data_wrapper()
+
+    net = network2.Network([784, 30, 10], cost=network2.CrossEntropyCost)
+    net.large_weight_initializer()
+    net.SGD(training_data, 30, 10, 0.1, lmbda = 5.0,
+            evaluation_data=validation_data,
+            monitor_evaluation_accuracy=True)        
+'''    
+
+'''
+import network3
+from network3 import Network
+from network3 import ConvPoolLayer, FullyConnectedLayer, SoftmaxLayer
+
+if __name__ == '__main__':
+    training_data, validation_data, test_data = network3.load_data_shared()
+    mini_batch_size = 10
+    net = Network([FullyConnectedLayer(n_in=784, n_out=100),
+            SoftmaxLayer(n_in=100, n_out=10)], mini_batch_size)
+    net.SGD(training_data, 60, mini_batch_size, 0.1, 
+                validation_data, test_data)
+'''
+
+import network3
+from network3 import Network
+from network3 import ConvPoolLayer, FullyConnectedLayer, SoftmaxLayer
+
+if __name__ == '__main__':
+    training_data, validation_data, test_data = network3.load_data_shared()
+    mini_batch_size = 10
+    net = Network([
+            ConvPoolLayer(image_shape=(mini_batch_size, 1, 28, 28),
+                          filter_shape=(20, 1, 5, 5),
+                          poolsize=(2,2)),
+            FullyConnectedLayer(n_in=20*12*12, n_out=100),
+            SoftmaxLayer(n_in=100, n_out=10)], mini_batch_size)
+    net.SGD(training_data, 60, mini_batch_size, 0.1, 
+                validation_data, test_data)
+    
+'''
+import network3
+from network3 import Network
+from network3 import ConvPoolLayer, FullyConnectedLayer, SoftmaxLayer
+
+if __name__ == '__main__':    
+    training_data, validation_data, test_data = network3.load_data_shared()
+    mini_batch_size = 10
+    
+    net = Network([
+            ConvPoolLayer(image_shape=(mini_batch_size, 1, 28, 28),
+                          filter_shape=(20, 1, 5, 5),
+                          poolsize=(2, 2)),
+            ConvPoolLayer(image_shape=(mini_batch_size, 20, 12, 12),
+                          filter_shape=(40, 20, 5, 5),
+                          poolsize=(2,2)),
+            FullyConnectedLayer(n_in=40*4*4, n_out=100),
+            SoftmaxLayer(n_in=100, n_out=10)], mini_batch_size)
+    net.SGD(training_data, 60, mini_batch_size, 0.03,
+                    validation_data, test_data)
+'''
+
+'''
+import network3
+from network3 import Network
+from network3 import ConvPoolLayer, FullyConnectedLayer, SoftmaxLayer
+from network3 import ReLU
+
+if __name__ == '__main__':    
+    training_data, validation_data, test_data = network3.load_data_shared()
+    mini_batch_size = 10
+    
+    net = Network([
+            ConvPoolLayer(image_shape=(mini_batch_size, 1, 28, 28),
+                          filter_shape=(20, 1, 5, 5),
+                          poolsize=(2, 2),
+                          activation_fn=ReLU),
+            ConvPoolLayer(image_shape=(mini_batch_size, 20, 12, 12),
+                          filter_shape=(40, 20, 5, 5),
+                          poolsize=(2,2),
+                          activation_fn=ReLU),
+            FullyConnectedLayer(n_in=40*4*4, n_out=100, activation_fn=ReLU),
+            SoftmaxLayer(n_in=100, n_out=10)], mini_batch_size)
+    net.SGD(training_data, 60, mini_batch_size, 0.03,
+                    validation_data, test_data, lmbda=0.1)
+'''            
+   
+        
+   
--- network3.py
+++ network3.py
@@ -62,7 +62,7 @@ class Network(object):
         self.x = T.matrix("x")
         self.y = T.ivector("y")
         init_layer = self.layers[0]
-        init_layer.set_input(self.x, self.x, self.mini_batch_size)
+        init_layer.set_inpt(self.x, self.x, self.mini_batch_size)
         for j in range(1, len(self.layers)):
             pre_layer, layer = self.layers[j-1], self.layers[j]
             layer.set_inpt(pre_layer.output, pre_layer.output_dropout, self.mini_batch_size)
@@ -136,26 +136,31 @@ class Network(object):
         
 class ConvPoolLayer(object):
     def __init__(self, filter_shape, image_shape, poolsize=(2,2),
-                 activaion_fn=sigmoid):
+                 activation_fn=sigmoid):
         self.filter_shape = filter_shape
         self.image_shape = image_shape
         self.poolsize = poolsize
         self.activation_fn = activation_fn
         
-        n_out = (filter_shape[0]*np.prod(filter_shape[2:]).np.prod(poolsize))
+        n_out = (filter_shape[0]*np.prod(filter_shape[2:])/np.prod(poolsize))
         self.w = theano.shared(
                 np.asarray(
                     np.random.normal(loc=0,scale=np.sqrt(1.0/n_out), size=filter_shape),
                     dtype=theano.config.floatX),
                     borrow=True)
+        self.b = theano.shared(
+                np.asarray(
+                        np.random.normal(loc=0, scale=1.0, size=(filter_shape[0],)),
+                        dtype=theano.config.floatX),
+                    borrow=True)
         self.params = [self.w, self.b]
         
-    def set_input(self, inpt, inpt_droppiut, mini_batch_size):
+    def set_inpt(self, inpt, inpt_droppiut, mini_batch_size):
         self.inpt = inpt.reshape(self.image_shape)
         conv_out = conv.conv2d(
-                input=self.inpt, filter=self.w, filter_shape=self.filter_shape,
+                input=self.inpt, filters=self.w, filter_shape=self.filter_shape,
                 image_shape=self.image_shape)
-        pooled_out = downsample.max_pool_2d(
+        pooled_out = pool_2d(
                 input=conv_out, ds=self.poolsize, ignore_border=True)
         self.output = self.activation_fn(
                 pooled_out + self.b.dimshuffle('x', 0, 'x', 'x'))
@@ -181,7 +186,7 @@ class FullyConnectedLayer(object):
                            name='b', borrow=True)
         self.params = [self.w, self.b]
     
-    def set_input(self, inpt, inpt_dropout, mini_batch_size):
+    def set_inpt(self, inpt, inpt_dropout, mini_batch_size):
         self.inpt = inpt.reshape((mini_batch_size, self.n_in))
         self.output = self.activation_fn(
                 (1-self.p_dropout)*T.dot(self.inpt, self.w) + self.b)
